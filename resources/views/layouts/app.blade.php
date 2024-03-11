4dd6c1d06f46e20a53283d8068e6119fc494f336
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-11 12:17:32 +0700

    login page
    login page ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -77,4 +77,65 @@
         </main>
     </div>
 </body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Sampah</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('stisla/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('stisla/modules/fontawesome/css/all.min.css') }}">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('stisla/modules/bootstrap-social/bootstrap-social.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('stisla/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('stisla/css/components.css') }}">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA -->
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                @yield('content')
+            </div>
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ asset('stisla/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/popper.js') }}"></script>
+    <script src="{{ asset('stisla/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('stisla/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('stisla/js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="{{ asset('stisla/js/scripts.js') }}"></script>
+    <script src="{{ asset('stisla/js/custom.js') }}"></script>
+</body>
+
 </html>
