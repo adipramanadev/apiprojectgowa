7f16666225f58673b94c1e41d3629aad4f73d68c
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-11 12:32:51 +0700

    dashboard page
    dashboard page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,7 +9,7 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="dropdown">
-                <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="{{ route('home') }}" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
 
             </li>
             <li class="menu-header">Data Manajemen User</li>
--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,26 @@ Auth::routes();
 
 // add prefix to the route admin 
 Route::prefix('admin')->group(function () {
+    // add middleware to the route
+    Route::middleware(['auth'])->group(function () {
+        #category
+        Route::get('category', [CategoryController::class, 'index'])->name('category.index');
+        Route::get('category/create', [CategoryController::class, 'create'])->name('category.create');
+        Route::post('category/store', [CategoryController::class, 'store'])->name('category.store');
+        Route::get('category/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+        Route::put('category/{category}/update', [CategoryController::class, 'update'])->name('category.update');
+        Route::delete('category/{id}/delete', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+        #role
+        Route::get('role', [RoleController::class, 'index'])->name('role.index');
+        Route::get('role/create', [RoleController::class, 'create'])->name('role.create');
+        Route::post('/role/store', [RoleController::class, 'store'])->name('role.store');
+        Route::delete('/role/{role}/destroy', [RoleController::class, 'destroy'])->name('role.destroy');
+        Route::get('/role/{role}/edit', [RoleController::class, 'edit'])->name('role.edit');
+        Route::put('/role/update/{role}', [RoleController::class, 'update'])->name('role.update');    
+    });
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    #category
-    Route::get('category', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('category/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::post('category/store', [CategoryController::class, 'store'])->name('category.store');
-    Route::get('category/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::put('category/{category}/update', [CategoryController::class, 'update'])->name('category.update');
-    Route::delete('category/{id}/delete', [CategoryController::class, 'destroy'])->name('category.destroy');
-
-    #role
-    Route::get('role', [RoleController::class, 'index'])->name('role.index');
-    Route::get('role/create', [RoleController::class, 'create'])->name('role.create');
-    Route::post('/role/store', [RoleController::class, 'store'])->name('role.store');
-    Route::delete('/role/{role}/destroy', [RoleController::class, 'destroy'])->name('role.destroy');
-    Route::get('/role/{role}/edit', [RoleController::class, 'edit'])->name('role.edit');
-    Route::put('/role/update/{role}', [RoleController::class, 'update'])->name('role.update');
+    
 });
 
