f142b9730b27496caa01c8f377afa31eaed55fa2
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-11 21:53:42 +0700

    item create
    item create

--- resources/views/admin/items/create.blade.php
+++ resources/views/admin/items/create.blade.php
@@ -26,9 +26,42 @@
                         <div class="card-body">
                             <form action="" method="post">
                                 @csrf
+                                @method('POST')
                                 <div class="form-group">
                                     <label for="name">Nama Item</label>
-                                    <input type="text" name="name" class="form-control" id="name" required>
+                                    <input type="text" name="name" class="form-control"
+                                        placeholder="Masukkan Nama Item..." id="name" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="categori_id">Nama Kategori</label>
+                                    <select name="categori_id" class="form-control" id="categori_id">
+                                        <option value="">Pilih Kategori</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="user_id">Nama User</label>
+                                    <select name="user_id" class="form-control" id="user_id">
+                                        <option value="">Pilih User</option>
+                                        @foreach ($users as $item)
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="weight">Weight</label>
+                                    <input type="number" name="weight" class="form-control"
+                                        placeholder="Masukkan Berat Item..." id="weight" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="points">Points</label>
+                                    <input type="number" name="points" class="form-control"
+                                        placeholder="Masukkan Points Item..." id="points" required>
+                                </div>
+                                <div class="form-group text-right">
+                                    <button class="btn btn-outline-primary ">Simpan Data</button>
                                 </div>
                             </form>
                         </div>
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -8,17 +8,20 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown">
-                <a href="{{ route('home') }}" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <li class="dropdown ">
+                {{-- jika di klik link active --}}
+
+                <a href="{{ route('home') }}" class="nav-link {{ request()->is('home') ? 'active' : '' }}"><i
+                        class="fas fa-fire"></i><span>Dashboard</span></a>
 
             </li>
             <li class="menu-header">Data Manajemen User</li>
-            <li class="dropdown">
+            <li class="dropdown" >
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Data User</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="#">Data User</a></li>
-                    <li><a class="nav-link" href="{{ route('role.index') }}">Atur Role</a></li>
+                    <li><a class="nav-link " href="#">Data User</a></li>
+                    <li><a class="nav-link {{ request()->is('role/*') ? 'active' : '' }}" href="{{ route('role.index') }}">Atur Role</a></li>
                 </ul>
             </li>
 
